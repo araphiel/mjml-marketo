e29bb56e2452d7b928d12521871211887f5a054e
Armon Raphiel <araphiel@gmail.com>
2020-07-04 15:01:57 -0500

    add validation

--- packages/mj-marketo-container/src/index.js
+++ packages/mj-marketo-container/src/index.js
@@ -1,4 +1,15 @@
 import { BodyComponent } from 'mjml-core'
+import { registerDependencies } from 'mjml-validator'
+
+registerDependencies({
+    // Tell the validator which tags are allowed as our component's children
+    'mj-marketo-container': [
+      'mj-section',
+      'mj-wrapper',
+    ],
+    // Now tell the validator which tags are allowed as our component's parent
+    'mj-body': ['mj-marketo-container'],
+  })
 
 export default class MjMarketoContainer extends BodyComponent {
     static endingTag = true
@@ -15,7 +26,7 @@ export default class MjMarketoContainer extends BodyComponent {
                     'class': 'mktoContainer'
                 })}
             >
-                ${this.renderChildren()}
+                ${this.renderChildren(this.props.children)}
             </table>
             `
     }
